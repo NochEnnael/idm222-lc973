280a57c3118cc0bfdeb14c14db274240c21e9970
NochEnnael <55722322+NochEnnael@users.noreply.github.com>
2020-03-20 23:56:31 -0400

    Final Edits

---
+++ classnotes/idm222_class02_lc973/testing.js
@@ -0,0 +1,19 @@
+function add(x, y){
+    return x + y;
+}
+
+add (1, 5);
+
+console.log (add (1, 5));
+
+//
+
+const cars=["Ford", "BMW", "Tesla", "Honda"]
+
+function buyCars(){
+    console.log (`I want a ${cars[2]}`);
+}
+
+buyCars();
+
+//
